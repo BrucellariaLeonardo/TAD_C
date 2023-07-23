484376539ba060e380741d6c56038d94a5ee38cb
BrucellariaLeonardo <brucellarialeonardo@gmail.com>
2023-07-22 22:13:51 -0300

    Implementacion de funcionalidades basicas de nodo.c

--- ListaEnlazada/nodo.c
+++ ListaEnlazada/nodo.c
@@ -2,12 +2,12 @@
 
 struct NodoTest
 {
-    int dato;
+    DatoT dato;
     struct NodoTest *pNext;
 };
 
 
-NodoT* crearNodo(int dato)  //recibe el dato a almacenar y devuelve la direccion al nodo donde se almaceno el dato, si se falla creando el nodo se retorna NULL
+NodoT* newNodo(DatoT dato)  //recibe el dato a almacenar y devuelve la direccion al nodo donde se almaceno el dato, si se falla creando el nodo se retorna NULL
 {
     NodoT *nuevo = (NodoT*) malloc(sizeof(NodoT));
     if( nuevo != NULL)
@@ -18,15 +18,48 @@ NodoT* crearNodo(int dato)  //recibe el dato a almacenar y devuelve la direccion
     return nuevo;
 }
 
-int compararNodo(NodoT nodo1, NodoT nodo2)
+void printNodo(NodoT *nodo)
+{
+    printf("Dato:\t%d\n", nodo->dato);
+    return;
+}
+
+void setNext(NodoT *nodo, NodoT *next)
+{
+    nodo->pNext = next;
+    return;
+}
+
+NodoT* getNext(NodoT *nodo)
+{
+    return nodo->pNext;
+}
+
+void setDato(NodoT *nodo, DatoT dato)
+{
+    nodo->dato = dato;
+}
+
+DatoT getDato(NodoT *nodo)
+{
+    return nodo->dato;
+}
+
+int compNodo(NodoT *nodo1, NodoT *nodo2)
 {
     int ret = 0;
-    if(nodo1.dato > nodo2.dato)
+    if(nodo1->dato > nodo2->dato)
     {
         ret = 1;
-    }else if (nodo1.dato < nodo2.dato)
+    }else if (nodo1->dato < nodo2->dato)
     {
         ret = -1;
     }
     return ret;
 }
+
+void freeNodo(NodoT *nodo)
+{
+    free(nodo);
+}
+
--- ListaEnlazada/nodo.h
+++ ListaEnlazada/nodo.h
@@ -4,12 +4,46 @@
 #include <stdio.h>
 
 typedef struct NodoTest NodoT; //alias para el nombre del nodo, en caso de querer implementar otro nodo sse cambia NodoTest por el nombre del nodo nuevo
+typedef int DatoT; //alias para los campos de datos del nodo
 
-NodoT* crearNodo(int dato);  //recibe el dato a almacenar y devuelve la direccion al nodo donde se almaceno el dato, si se falla creando el nodo se retorna NULL
-int compararNodo(NodoT nodo1, NodoT nodo2);
-/*Retorna 1 en caso de que nodo1 sea mayor
-  Retorna 0 en caso de que los nodos sean iguales
-  Retorna -1 en caso de que nodo2 sea mayor
-*/
+/*Crea un nodo y retorna su direccion de memoria
+@param dato dato a guardar en el nodo
+@return direccion del nodo creado, en caso de no haber creado el nodo retorna NULL*/
+NodoT* newNodo(DatoT dato);  
 
+/*Muestra la informacion de un nodo
+@param *nodo direccion del nodo que se desea mostrar*/
+void printNodo(NodoT *nodo);
+
+/*Cambia la direccion a la que apunta el nodo
+@param *nodo direccion del nodo que se quiere editar
+@param *next direccion del nodo al que se quiere apuntar*/
+void setNext(NodoT *nodo, NodoT *next);
+
+/*obtine la direccion a la que apunta el nodo recibido
+@param *nodo direccion del nodo que se quiere consultar
+@return nodo al que apunta el nodo consultado*/
+NodoT* getNext(NodoT *nodo);
+
+/*Cambia el dato almacenado por el nodo
+@param *nodo direccion del nodo que se quiere editar
+@param dato dato a guardar*/
+void setDato(NodoT *nodo, DatoT dato);
+
+/*obtine el dato almacenado por un nodo
+@param *nodo direccion del nodo que se quiere consultar
+@return dato del nodo*/
+DatoT getDato(NodoT *nodo);
+
+/*compara dos nodos
+@param nodo1 direccion del primer nodo a comparar
+@param nodo2 direccion del segundo nodo a comprar
+@return 1 en caso de que nodo1 sea mayor
+@return 0 en caso de que los nodos sean iguales
+@return -1 en caso de que nodo2 sea mayor*/
+int compNodo(NodoT *nodo1, NodoT *nodo2);
+
+/*libera le memoria utilizada por un nodo
+@param *nodo direccion del nodo a eliminar*/
+void freeNodo(NodoT *nodo);
 #endif
